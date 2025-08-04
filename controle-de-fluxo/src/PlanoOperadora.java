69e63e457f902c53124c77cf256fde42953b3692
Marcello Carnio <marcellocarnio@gmail.com>
2025-08-04 15:38:51 -0300

    Atualização da Aula

---
+++ controle-de-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,20 @@
+public class PlanoOperadora {
+    
+    public static void main(String[] args) {
+        
+        String plano = "M";
+
+        switch (plano) {
+            case "T":
+                System.out.println("5GB de Youtube");
+                
+            case "M":
+                System.out.println("Whats e Instragam gratis");
+                
+            case "B":
+                System.out.println("100min de ligações");
+                
+               
+            }
+    }
+}
